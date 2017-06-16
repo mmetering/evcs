8faa3db846b71f6eb70fc7616086c7d3b260b9fe
chrisonntag <sonntagc@fim.uni-passau.de>
2017-06-16 15:45:32 +0200

    Add the EVCSData model

--- models.py
+++ models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from mmetering.models import Flat
 
-# Create your models here.
+
+class EVCSData(models.Model):
+    flat = models.ForeignKey(Flat)
+    saved_time = models.DateTimeField()
+    value = models.FloatField()
+
+    def __str__(self):
+        return "EVCS-Datenwert für " + self.flat.name
+
+    class Meta:
+        permissions = (
+            ("can_download", "Can download EVCSData"),
+            ("can_view", "Can view EVCSData"),
+        )
