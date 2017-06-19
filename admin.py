e74d4c3b95abc5e02915c86d624022df3644ec11
chrisonntag <sonntagc@fim.uni-passau.de>
2017-06-19 23:07:16 +0200

    Add models for RFID and charging start/end values

--- admin.py
+++ admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from evcs.models import RFID
 
-# Register your models here.
+
+admin.site.register(RFID)
--- models.py
+++ models.py
@@ -2,16 +2,41 @@ from django.db import models
 from mmetering.models import Flat
 
 
-class EVCSData(models.Model):
+class RFID(models.Model):
+    """
+    Models the RFID chip and creates adds a relation to a certain flat.
+    """
+    serial = models.CharField(max_length=128)
     flat = models.ForeignKey(Flat)
-    saved_time = models.DateTimeField()
+
+    def __str__(self):
+        return "RFID von " + self.flat.name
+
+    class Meta:
+        verbose_name = "RFID"
+        verbose_name_plural = "RFIDs"
+
+
+class ChargingStartValue(models.Model):
+    """
+    Saves the meter value at the start of the charging process.
+    """
+    rfid = models.ForeignKey(RFID, verbose_name='RFID')
+    time = models.DateTimeField()
     value = models.FloatField()
 
     def __str__(self):
-        return "EVCS-Datenwert für " + self.flat.name
+        return "EVCS-Startwert für " + self.rfid.flat.name
 
-    class Meta:
-        permissions = (
-            ("can_download", "Can download EVCSData"),
-            ("can_view", "Can view EVCSData"),
-        )
+
+class ChargingEndValue(models.Model):
+    """
+    Saves the meter value at the end of the charging process.
+    """
+    rfid = models.ForeignKey(RFID)
+    start = models.ForeignKey(ChargingStartValue, on_delete=models.CASCADE)
+    time = models.DateTimeField()
+    value = models.FloatField()
+
+    def __str__(self):
+        return "EVCS-Endwert für " + self.rfid.flat.name
